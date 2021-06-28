11843c063641cbcf75f93c5d3c867185604dcf4f
Abolfazl Panahiazar <abolfazlpanahiazar@gmail.com>
2021-06-28 08:50:40 +0430

    Updates

---
+++ precache-manifest.77b7cfddb93a8e4c8124304cff3a6a8b.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cddccdf2faaf1d43cc39f01d07881f9d",
+    "url": "/Persian-Material-UI-admin-panel/index.html"
+  },
+  {
+    "revision": "b700fb83166b6cb27b72",
+    "url": "/Persian-Material-UI-admin-panel/static/css/2.dd1d7839.chunk.css"
+  },
+  {
+    "revision": "b700fb83166b6cb27b72",
+    "url": "/Persian-Material-UI-admin-panel/static/js/2.9250618d.chunk.js"
+  },
+  {
+    "revision": "c9ddb0149f9be894234f",
+    "url": "/Persian-Material-UI-admin-panel/static/js/main.27a05f99.chunk.js"
+  },
+  {
+    "revision": "58f07c32555dee45b241",
+    "url": "/Persian-Material-UI-admin-panel/static/js/runtime~main.0dbed931.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Persian-Material-UI-admin-panel/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Persian-Material-UI-admin-panel/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Persian-Material-UI-admin-panel/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Persian-Material-UI-admin-panel/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Persian-Material-UI-admin-panel/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "09aea0f59807f6f4f66af7f5719cba9e",
+    "url": "/Persian-Material-UI-admin-panel/static/media/google.09aea0f5.svg"
+  },
+  {
+    "revision": "3d432ca2badb7e0130b379f9162b99b1",
+    "url": "/Persian-Material-UI-admin-panel/static/media/logo.3d432ca2.svg"
+  },
+  {
+    "revision": "ca0a320670764c65ff72142685f57aa6",
+    "url": "/Persian-Material-UI-admin-panel/static/media/logo.ca0a3206.png"
+  }
+]);
